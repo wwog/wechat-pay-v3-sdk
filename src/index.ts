ea21eaf0d963f86af73278f693ec6052dc8032ba
wwog <871782513@qq.com>
2023-03-21 02:06:09 +0800

    npm相关

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 export type { ContainerOptions, WechatBaseEventOPtions, WechatBaseOptions } from './base'
-export { getInstances, WechatPayV3Base } from './base'
+export { getInstances, WechatPayV3Base, apiContainer } from './base'
 export { Applyment } from './apis/applyment/applyment'
 export { JSAPI } from './apis/basePay/JSAPI'
 
-export * as utils from './utils'
-import { apiContainer } from './base'
-export { apiContainer }
-export default apiContainer
+export * from './utils'
